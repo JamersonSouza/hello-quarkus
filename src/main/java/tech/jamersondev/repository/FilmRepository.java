e3e1dd76bdb2a02d28cebf9100a33e5ee21321ad
jamersonsouza <jamersonsouza@outlook.com>
2023-11-05 14:51:38 -0300

    Classe de repoisoty e método para pegar um filme por id

--- src/main/java/tech/jamersondev/repository/FilmRepository.java
+++ src/main/java/tech/jamersondev/repository/FilmRepository.java
@@ -1,2 +1,23 @@
-package tech.jamersondev.repository;public interface FilmRepository {
+package tech.jamersondev.repository;
+
+import com.speedment.jpastreamer.application.JPAStreamer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import tech.jamersondev.model.Film;
+import tech.jamersondev.model.Film$;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class FilmRepository {
+
+    @Inject
+    JPAStreamer jpaStreamer;
+
+    public Optional<Film> getFilm(short filmId){
+        return jpaStreamer.stream(Film.class)
+                .filter(Film$.filmId.equal(filmId))
+                .findFirst();
+    }
+
 }
