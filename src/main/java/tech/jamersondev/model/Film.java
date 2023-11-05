55f265e97eef88ee2e8c94937307ea193159cfab
jamersonsouza <jamersonsouza@outlook.com>
2023-11-05 10:48:54 -0300

    Modelagem do objeto filme com base no db sakila

---
+++ src/main/java/tech/jamersondev/model/Film.java
@@ -0,0 +1,175 @@
+package tech.jamersondev.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "film", schema = "sakila")
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "film_id")
+    private short filmId;
+
+    @Basic
+    @Column(name = "title")
+    private String title;
+
+    @Basic
+    @Column(name = "description")
+    private String description;
+
+    @Basic
+    @Column(name = "language_id")
+    private short languageId;
+
+    @Basic
+    @Column(name = "original_language_id")
+    private Short originalLanguageId;
+
+    @Basic
+    @Column(name = "rental_duration")
+    private Short rentalDuration;
+
+
+    @Basic
+    @Column(name = "rental_rate", columnDefinition = "decimal(4,2)")
+    private Float rentalRate;
+
+    @Basic
+    @Column(name = "length")
+    private Short length;
+
+    @Basic
+    @Column(name = "replacement_coast")
+    private BigDecimal replacementCoast;
+
+
+    @Basic
+    @Column(name = "rating", columnDefinition = "enum('G', 'PG', 'PG-13', 'R', 'NC-17')")
+    private String rating;
+
+    @Basic
+    @Column(name = "special_features", columnDefinition = "set('Trailers', 'Commentaries', 'Deleted Scenes', 'Behind the Scenes')")
+    private String specialFeatures;
+
+    @Basic
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public Film() {
+    }
+
+    public short getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(short filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public short getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(short languageId) {
+        this.languageId = languageId;
+    }
+
+    public Short getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(Short originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public Short getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(Short rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public Float getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(Float rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Short getLength() {
+        return length;
+    }
+
+    public void setLength(Short length) {
+        this.length = length;
+    }
+
+    public BigDecimal getReplacementCoast() {
+        return replacementCoast;
+    }
+
+    public void setReplacementCoast(BigDecimal replacementCoast) {
+        this.replacementCoast = replacementCoast;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(String specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return filmId == film.filmId && languageId == film.languageId && Objects.equals(title, film.title) && Objects.equals(description, film.description) && Objects.equals(originalLanguageId, film.originalLanguageId) && Objects.equals(rentalDuration, film.rentalDuration) && Objects.equals(rentalRate, film.rentalRate) && Objects.equals(length, film.length) && Objects.equals(replacementCoast, film.replacementCoast) && Objects.equals(rating, film.rating) && Objects.equals(specialFeatures, film.specialFeatures) && Objects.equals(lastUpdate, film.lastUpdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filmId, title, description, languageId, originalLanguageId, rentalDuration, rentalRate, length, replacementCoast, rating, specialFeatures, lastUpdate);
+    }
+}
