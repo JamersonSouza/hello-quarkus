e41ac07eaeca3d78703a18dcd231fcee44a7cd2b
jamersonsouza <jamersonsouza@outlook.com>
2023-11-05 15:19:45 -0300

    Endpoints para buscar filmes paginados e por tamanhos

--- src/main/java/tech/jamersondev/resources/FilmResource.java
+++ src/main/java/tech/jamersondev/resources/FilmResource.java
@@ -5,11 +5,11 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import tech.jamersondev.exceptions.ExceptionNotFoundFilm;
 import tech.jamersondev.model.Film;
 import tech.jamersondev.repository.FilmRepository;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/film")
 public class FilmResource {
@@ -23,6 +23,24 @@ public class FilmResource {
     public String getFilme(short filmId){
         Optional<Film> film = filmRepository.getFilm(filmId);
         return film.isPresent() ? film.get().getTitle() : "Filme não encontrado";
+    }
+
+    @GET
+    @Path("/pagedFilms/{page}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String filmsPaged(long page, short minLength){
+            return filmRepository.listFilmPaged(page, minLength)
+                    .map(f -> String.format("%s (%d min)", f.getTitle(), f.getLength()))
+                    .collect(Collectors.joining("\n")); //operador de junção das collections
+    }
+
+    @GET
+    @Path("/AllFilms/{maxLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAllFilms(short maxLength){
+        return filmRepository.listAllFilms(maxLength)
+                .map(f -> String.format("%s (%d max)", f.getTitle(), f.getLength()))
+                .collect(Collectors.joining("\n"));
     }
 
 }
